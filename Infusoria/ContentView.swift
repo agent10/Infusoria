d77309222a7632af5b2a746561d9a565cc364ec4
Kirill Olenyov <olenyov.kirill@me.com>
2019-12-20 14:49:39 +0300

    Initial Commit

---
+++ Infusoria/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Infusoria
+//
+//  Created by Kirill Olenyov on 20.12.2019.
+//  Copyright © 2019 Kirill Olenyov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
